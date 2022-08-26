39c586ce25e75e2557c14d5367bc66656d25c6b3
Sebastian Müller <mueller.seb@posteo.de>
2022-08-26 14:23:04 +0200

    update cmake args for arm64 builds

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export FC=$(basename ${FC})
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_WITH_OpenMP=ON -B build -S .
+cmake ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -DCMAKE_WITH_OpenMP=ON -B build -S .
 cmake --build build --parallel
 cmake --install build --prefix $PREFIX
